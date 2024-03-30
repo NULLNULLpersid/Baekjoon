5a3a5f22132e130e4d335ffaa0f9e5c4ee7417e5
NULLNULLpersid <persid@cu.ac.kr>
2024-03-31 05:10:59 +0900

    test

--- C99/test.c
+++ C99/test.c
@@ -1,4 +1,4 @@
-"""
+/*
 #include <stdio.h>
 #define SIZE 10
 
@@ -132,4 +132,65 @@ int main(void) {
     } while(choice != 4);
     return 0;
 }
-"""
+*/
+#include <stdio.h>
+#define SIZE 10
+typedef char *element;
+typedef struct {
+    element arr[SIZE];
+    int front, rear;
+} QueueType;
+
+void init(QueueType *q){
+    for (int i = 0; i < SIZE; i++) q->arr[i] = NULL;
+	q->front = q->rear = 0;
+}
+
+int full(QueueType* q) {
+	return q->rear%SIZE == q->front;
+}
+int empty(QueueType *q){
+	return q->front == q->rear;
+}
+void enqueue(QueueType *q, char *u) {
+    if (!full(q)) printf("대기열이 가득 찼습니다.\n");
+    else q->arr[++q->rear%SIZE] = u;
+}
+element dequeue(QueueType *q) {
+    if (empty(q)) {
+        printf("대기열이 비어 있습니다.\n");
+        return NULL;
+    } else return q->arr[++q->front%SIZE];
+}
+void print(QueueType *q) {
+    int i = (q->front + 1) % SIZE;
+    while (i != (q->rear + 1) % SIZE) {
+        (q->arr[i] != NULL) ? printf("%s", q->arr[i]) : printf(", ");
+        i = (i + 1) % SIZE;
+    }
+    printf("\n");
+}
+
+int main(void) {
+    QueueType q;
+    int choice;
+    char name[SIZE];
+    init(&q);
+    do {
+        scanf("%d", &choice);
+        switch (choice) {
+        case 1:
+            scanf("%s", name);
+            enqueue(&q, name);
+            break;
+        case 2:
+            dequeue(&q);
+            break;
+        case 3:
+            print(&q);
+            break;
+        default: printf("잘못입력하셨습니다.");
+        }
+    } while(choice != 4);
+    return 0;
+}
