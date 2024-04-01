330126c2785f883dedded155eb7e951b3987b0eb
NULLNULLpersid <persid@cu.ac.kr>
2024-04-01 11:05:45 +0900

    test

---
+++ python/test.py
@@ -0,0 +1,17 @@
+def printf(N):
+    for i in range(N):
+        for j in range(N):
+            print("{}".format(arr[i][j]),end=' ')
+        print("")
+
+
+
+
+N = int(input())
+size = N*N
+arr = [[0 for i in range(N)] for j in range(N)]
+
+
+
+
+
