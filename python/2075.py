40d606a5e1847daf0c49f4debde92e6848714c4a
NULLNULLpersid <persid@cu.ac.kr>
2024-04-08 08:01:39 +0900

    commit

--- python/2075.py
+++ python/2075.py
@@ -1,7 +1,16 @@
+"""
+Chat GPT를 활용한 코드
+import heapq
+n = int(input())
+heap = []
+for _ in range(n):
+    for x in map(int, input().split()):
+        heapq.heappush(heap, x)
+        if len(heap) > n:
+            heapq.heappop(heap)
+print(heap[0])
+"""
 import sys
 n = int(input())
-l=[]
 for i in range(n):
-    l.append(list(map(int, sys.stdin.readline().split())))
-    l.sort()
-    print(l)
+    list(map(int, sys.stdin.readline().split()))
